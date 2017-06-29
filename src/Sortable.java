7f80b9cdc533fb89815eef9041db0a1a384b2719
kruglikalexey <kruglikalexey@ya.ru>
2017-06-29 09:09:40 +0300

    final version

--- src/Sortable.java
+++
@@ -1,9 +0,0 @@
-import java.util.Vector;
-
-/**
- * Created by root on 22.06.17.
- */
-public interface Sortable {
-    Vector<Integer> sort(int step);
-    void swap(int[] array, int left, int right);
-}
---
+++ src/vis/sortvisualizer/sorts/Sortable.java
@@ -0,0 +1,11 @@
+package vis.sortvisualizer.sorts;
+
+import java.util.Vector;
+
+/**
+ * Created by root on 22.06.17.
+ */
+public interface Sortable {
+    Vector<Integer> sort(int step);
+    void swap(int[] array, int left, int right);
+}
